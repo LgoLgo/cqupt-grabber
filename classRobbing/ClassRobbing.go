bdf12c128e50979236cdbcb63d8f2fe091772c11
L2ncE <llance_24@foxmail.com>
2022-05-13 12:13:18 +0800

    refactor:重构抢课相关

--- classRobbing/ClassRobbing.go
+++ classRobbing/ClassRobbing.go
@@ -54,6 +54,49 @@ func SingleRob(cookie string, load string) string {
 	return Response.Info
 }
 
+//SingleRobWithInfo 仅抢课一次，传递单个load以及cookie，打印Info
+func SingleRobWithInfo(cookie string, load string) {
+	client := &http.Client{}
+	var data = strings.NewReader(load)
+	req, err := http.NewRequest("POST", "http://xk1.cqupt.edu.cn/post.php", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("Cookie", cookie)
+	req.Header.Set("Origin", "http://xk1.cqupt.edu.cn")
+	req.Header.Set("Referer", "http://xk1.cqupt.edu.cn/yxk.php")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}(resp.Body)
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode:", resp.StatusCode, "body", string(bodyText))
+	}
+	var Response model.Response
+	err = json.Unmarshal(bodyText, &Response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf(Response.Info)
+}
+
 //LoopRob 循环抢课，支持多个课程同时抢，每次请求停顿0.2秒，防止被ban。
 //传入一个cookie和一个load切片
 func LoopRob(cookie string, loads []string) {
@@ -64,7 +107,7 @@ func LoopRob(cookie string, loads []string) {
 
 			//调用SingleRob进行循环抢课
 
-			info := SingleRob(load, cookie)
+			info := SingleRob(cookie, load)
 			if info == "ok" {
 				log.Printf("课程%d：%s\n", j, info)
 				goto ok
