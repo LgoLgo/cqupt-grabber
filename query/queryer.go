76cabbab4269134a574d8c745f9659cacb1ff18b
ViolaPioggia <2604296771@qq.com>
2023-05-06 20:21:54 +0800

    fix a tiny bug

--- query/queryer.go
+++ query/queryer.go
@@ -50,8 +50,8 @@ func (q *Queryer) AllRenWen(cookie string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var builder strings.Builder
 	for _, item := range classInfo.Data {
-		var builder strings.Builder
 		SRsLimit := strconv.Itoa(item.RsLimit)
 		SRwType := strconv.Itoa(item.RwType)
 
@@ -64,11 +64,12 @@ func (q *Queryer) AllRenWen(cookie string) {
 		for _, str := range strs {
 			builder.WriteString(str)
 		}
-		loads := builder.String()
-		err = ioutil.WriteFile("./output_renwen.txt", []byte(loads), 0o666) // 写入文件(字节数组)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+		builder.WriteString("\n")
+	}
+	loads := builder.String()
+	err = ioutil.WriteFile("./output_renwen.txt", []byte(loads), 0o666) // 写入文件(字节数组)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 	return
 }
@@ -80,8 +81,8 @@ func (q *Queryer) AllZiRan(cookie string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var builder strings.Builder
 	for _, item := range classInfo.Data {
-		var builder strings.Builder
 		SRsLimit := strconv.Itoa(item.RsLimit)
 		SRwType := strconv.Itoa(item.RwType)
 
@@ -94,11 +95,12 @@ func (q *Queryer) AllZiRan(cookie string) {
 		for _, str := range strs {
 			builder.WriteString(str)
 		}
-		loads := builder.String()
-		err = ioutil.WriteFile("./output_ziran.txt", []byte(loads), 0o666) // 写入文件(字节数组)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+		builder.WriteString("\n")
+	}
+	loads := builder.String()
+	err = ioutil.WriteFile("./output_ziran.txt", []byte(loads), 0o666) // 写入文件(字节数组)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 	return
 }
